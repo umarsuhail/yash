25708bf6fb4e5222832f708d91be792ee5ac6c4c
kapreign <umarsuhail112@gmail.com>
2022-04-25 11:48:19 +0530

    first commit

---
+++ src/components/RecipeList/RecipeList.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Tiles from '../Tiles/Tiles'
+import tile1 from '../../images/tile1.png'
+import tile3 from '../../images/tile3.png'
+import next from '../../images/next.svg'
+import previous from '../../images/previous.svg'
+
+import { Carousel, Container } from 'react-bootstrap'
+import './RecipeList.scss'
+export default function RecipeList() {
+    return (
+        <div className="recipe-wrapper">
+            <h1 className="latest-recipes">Latest Recipes</h1>
+            <div className="w-100 next-previous">
+                <img className="next " src={next}></img>
+                <img className="previous " src={previous}></img>
+            </div>
+            <Container>
+                <Carousel>
+                    <Carousel.Item>
+                        <Tiles image={tile1} content="Portobello Baked Eggs with avocado & bread" amount='€8'></Tiles>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Tiles image={tile3} content="Summery salmon, beetroot & spicy mix" amount='€15'></Tiles>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Tiles image={tile3} content="Portobello Baked Eggs with avocado & bread" amount='€8'></Tiles>
+                    </Carousel.Item>
+                </Carousel>
+            </Container>
+
+        </div>
+    )
+}
