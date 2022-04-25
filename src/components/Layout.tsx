644141c7439f7ef398fe3bef9d3d0d3d31142206
kapreign <umarsuhail112@gmail.com>
2022-04-25 15:09:46 +0530

    first commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavbarContainer from './Navbar/Navbar';
-import logo from '../images/logo.png';
+import logo from '../assets/images/logo.png';
 
 interface props {
   children: any
